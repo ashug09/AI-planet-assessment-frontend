dc869f6dcaeb43d3a00384fe14dd108a957c50fb
ashug09 <79792289+ashug09@users.noreply.github.com>
2024-06-20 02:05:01 +0530

    updated sidenav

--- src/pages/components/sideNav/sideNav.jsx
+++ src/pages/components/sideNav/sideNav.jsx
@@ -35,7 +35,13 @@ export default function SideNav() {
         </div>
       </div>
       {/* new chat button  */}
-      <div onClick={() => router.push("/")} className="flex justify-between mx-2 mb-2 rounded-xl px-5 py-2 cursor-pointer bg-gray-200 hover:bg-gray-300">
+      <div
+        onClick={() => {
+          router.push("/");
+          router.refresh();
+        }}
+        className="flex justify-between mx-2 mb-2 rounded-xl px-5 py-2 cursor-pointer bg-gray-200 hover:bg-gray-300"
+      >
         <IoAddCircleOutline size={28} />
         <h1 className="my-auto">New Chat</h1>
       </div>
